cfdf857dc86788273606d245f3ab3d1a714dc590
huangray93 <145305092+huangray93@users.noreply.github.com>
2023-11-24 15:51:05 +0800

    ex01

---
+++ CH12/Ch12_1.java
@@ -0,0 +1,11 @@
+package ex01;
+public class Ch12_1{
+    public static void main (String [] args){
+        Rectangle r1 = new Rectangle(5);
+        r1.width = 20;
+        r1.height = 15;
+        System.out.println("width="+r1.width);
+        System.out.println("height="+r1.height);
+
+    }
+}
---
+++ CH12/Rectangle.java
@@ -0,0 +1,9 @@
+package ex01;
+class Rectangle{
+    int width;
+    int height;
+    public Rectangle(int x)
+    {
+        System.out.println(x) ;
+    }
+}
